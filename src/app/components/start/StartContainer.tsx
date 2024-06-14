9e0f9af977f1fc16794abf2b76560f0f358edcd3
Minkyu0424 <yng1404@gmail.com>
2024-06-14 16:16:27 +0900

    :sparkles: 깃허브 연결

--- src/app/components/start/StartContainer.tsx
+++ src/app/components/start/StartContainer.tsx
@@ -1,9 +1,15 @@
+import Door from "../common/Door";
 import Welcome from "./Welcome";
 
 const StartContainer = () => {
   return (
-    <div className="flex justify-center ">
+    <div className="flex justify-center flex-col items-center relative">
       <Welcome />
+      <div className="flex gap-x-8 mt-20">
+        <Door doorTitle={"시작"} />
+        <Door doorTitle={"규칙"} />
+        <Door doorTitle={"기록"} />
+      </div>
     </div>
   );
 };
