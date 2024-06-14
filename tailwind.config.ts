2804a2334db683d11189c0fb7df81a0608dc9b5a
Minkyu0424 <yng1404@gmail.com>
2024-06-14 18:14:57 +0900

    :wrench: nextui 전역 설정

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,13 @@
 import type { Config } from "tailwindcss";
+const { nextui } = require("@nextui-org/react");
 
 const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@nextui-org/theme/dist/components/button.js",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
@@ -18,7 +21,8 @@ const config: Config = {
         wiggle: "wiggle 1s ease-in-out infinite",
       },
     },
-    plugins: [],
+    darkMode: "class",
+    plugins: [nextui()],
   },
 };
 export default config;
