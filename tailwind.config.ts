3bf8284faab98f84077b193f17bf369d575fc7e6
Minkyu0424 <yng1404@gmail.com>
2024-06-18 15:41:54 +0900

    📝 [docs]: NextUi 전역 설치

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,7 +6,6 @@ const config: Config = {
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./node_modules/@nextui-org/theme/dist/components/button.js",
     "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
@@ -21,7 +20,6 @@ const config: Config = {
         wiggle: "wiggle 1s ease-in-out infinite",
       },
     },
-    darkMode: "class",
     plugins: [nextui()],
   },
 };
