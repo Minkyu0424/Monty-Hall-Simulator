4cf31bbaaa797632036126f1e8bf4e0f56d86ce3
Minkyu0424 <yng1404@gmail.com>
2024-06-18 16:33:21 +0900

    :recycle: [refactor]: 시뮬레이션 옵션 types 지정

---
+++ src/app/_types/simulate.ts
@@ -0,0 +1,24 @@
+type sim_Door = 3 | 5 | 10 | 50;
+
+type sim_Count = 1 | 3 | 5 | 10 | 30;
+
+interface simulate_option {
+  turns: sim_Count;
+  doorAmount: sim_Door;
+  user: string;
+  onlyResult: boolean;
+  isPause?: boolean;
+}
+
+// 상단 6-70퍼센트 문으로 보여주기
+// 돌아가기 버튼 => 진행중이던 시뮬레이션이 초기화 된다는 경고창 On
+
+// 하단엔 입력창
+
+// 사용자 이름
+// 문 개수 => 드롭다운 3개, 5개, 50개 100개
+// 시뮬레이션 횟수 => 드롭다운 (자유, 1회, 3회 5회 10회)
+// 결과만 보기 모드 => On/Off
+
+// 옆에 시작하기 버튼
+// 리셋버튼도 함께 처음에 disabled
